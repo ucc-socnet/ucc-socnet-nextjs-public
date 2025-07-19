6b1bd8b1195261d8ccfe68d68c6a4b828944a1c0
tongaks <rrfrokuu@gmail.com>
2025-07-19 09:37:27 +0800

    initial (translating for nextjs compatibility)

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+import styles from './navbar.module.css';
+
+export default function Navbar() {
+	return (
+		<div className={styles.navbar}>
+			<div className={styles.logo}>USN</div>
+
+			<div className={styles.search_bar}>
+				<input type="text" placeholder="Search" />
+				<button><i className="fas fa-search"></i></button>
+			</div>
+
+			<div className={styles.nav}>
+				<a href="../main-page-example/index.html"><i className="fa-solid fa-house"></i></a>
+				<a href="#"><i className="fa-solid fa-user-group"></i></a>
+				<a href="#"><i className="fa-solid fa-people-group"></i></a>
+				<i className="fas fa-bell"></i>
+				<i className="fas fa-cog"></i>
+			</div>
+		</div>
+	);
+}
---
+++ app/test-page/page.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Home() {
+	return (
+		<div className="container">
+			<h1>Test</h1>
+		</div>
+	)
+}

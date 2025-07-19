54b4be548dff198fa8f814a64e52902f934c4512
tongaks <rrfrokuu@gmail.com>
2025-07-19 09:32:28 +0800

    initial (translating for nextjs compatibility)

---
+++ app/components/sidebar/sidebar.tsx
@@ -0,0 +1,17 @@
+import style from './sidebar.module.css'
+
+export default function Sidebar() {
+	return (
+
+	<aside className={style.sidebar}>
+      <ul className={style.navside_controls}>
+        <li><i className="fas fa-home"></i> Home</li>
+        <a href="../profile/index.html"><li><i className="fas fa-user"></i> Profile</li></a>
+        <li><i className="fas fa-user-friends"></i> Friends</li>
+        <li><i className="fas fa-heart"></i> My Likes</li>
+        <li><i className="fas fa-bookmark"></i> Bookmarks</li>
+      </ul>
+    </aside>
+
+	);
+}

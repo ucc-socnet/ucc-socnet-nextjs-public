cac3390a0030de2428301d90c85fc8d3545a3a46
tongaks <rrfrokuu@gmail.com>
2025-07-23 00:30:06 +0800

    pytangian

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
-const secret = new TextEncoder().encode(process.env.JWT_SECRET); // replace if using something else
+const secret = new TextEncoder().encode(process.env.SESSION_SECRET_KEY); // replace if using something else
 
 const protectedRoutes = ["/"];
 const publicRoutes = ["/login"];

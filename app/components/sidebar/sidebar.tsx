5db29af330c7391a20d8087ce9cb8e27b6775abf
tongaks <rrfrokuu@gmail.com>
2025-07-24 10:07:31 +0800

    working profile page

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
 	<aside className={style.sidebar}>
       <ul className={style.navside_controls}>
         <li><i className="fas fa-home"></i> Home</li>
-        <a href="/login"><li><i className="fas fa-user"></i> Profile</li></a>
+        <a href="/profile"><li><i className="fas fa-user"></i> Profile</li></a>
         <li><i className="fas fa-user-friends"></i> Friends</li>
         <li><i className="fas fa-heart"></i> My Likes</li>
         <li><i className="fas fa-bookmark"></i> Bookmarks</li>
